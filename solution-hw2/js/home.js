cc54459e05154ef54fbc6d04c39e129f246c1f30
Michael Auld <112352959+mauld-cmu@users.noreply.github.com>
2022-09-14 10:35:51 -0400

    Added HW1 Files and linked JS
    - Defined glazing & pack size objects

---
+++ solution-hw2/js/home.js
@@ -0,0 +1,14 @@
+const glazingOptions = {
+  keepOriginal: 0,
+  sugarMilk: 0,
+  vanillaMilk: 0.5,
+  doubleChocolate: 1.5
+}
+
+const packSizeOptions = {
+  onePack: 1,
+  threePack: 3,
+  sixPack: 5,
+  twelvePack: 10
+}
+
